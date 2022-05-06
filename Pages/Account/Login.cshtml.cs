4878cec87b001cd446309d9ab67ddb30812d9dad
Dumitru Golea <dgolea@techteam.com>
2022-05-06 21:00:52 +0300

    geberate cookie with Cookie Authentication Handler

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,12 +15,37 @@ namespace WebApp_IdentityServer4.Pages.Account
         public Credential Credential { get; set; }
         public void OnGet()
         {
+            
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+            
+            // Verify the credential
+            if (Credential.UserName == "admin" && Credential.Password == "password")
+            {
+                // Creating the security context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@mywebsite.com")
+                };
+                var identity= new ClaimsIdentity(claims,"MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+            return Page();
         }
     }
 
     public class Credential
     {
         [Required]
+        [Display(Name ="User Name")]
         public string UserName { get; set; }
 
         [Required]
--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,10 @@ namespace WebApp_IdentityServer4
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication().AddCookie("MyCookieAuth", options =>
+            {
+                options.Cookie.Name = "MyCookieAuth";
+            });
             services.AddRazorPages();
         }
 
